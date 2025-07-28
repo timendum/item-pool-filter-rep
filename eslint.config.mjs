2c866cc5e9b4645e60c14e287c611b4502d0753d
timendum <timedum@gmail.com>
2025-07-28 12:25:10 +0200

    chore: add files from IsaacScript 5.2.14 template

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+// This is the configuration file for ESLint, the TypeScript linter:
+// https://eslint.org/docs/latest/use/configure/
+
+// @ts-check
+
+import { completeConfigBase } from "eslint-config-complete";
+import { isaacScriptModConfigBase } from "eslint-config-isaacscript"; // eslint-disable-line import-x/no-extraneous-dependencies
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  // https://github.com/complete-ts/complete/blob/main/packages/eslint-config-complete/src/base.js
+  ...completeConfigBase,
+
+  // https://github.com/IsaacScript/isaacscript/blob/main/packages/eslint-config-isaacscript/src/base.js
+  ...isaacScriptModConfigBase,
+
+  {
+    rules: {
+      // Insert changed or disabled rules here, if necessary.
+    },
+  },
+);
